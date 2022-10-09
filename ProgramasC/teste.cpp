51b81b220e57293e529e2810e6765c4a1e3f0f04
lucasdelimagarreto <54108848+lucasdelimagarreto@users.noreply.github.com>
2022-10-09 13:03:40 -0300

    sad

---
+++ ProgramasC/teste.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int numOperacoes; 
+    scanf("%d", &numOperacoes);
+    int listaPresentes[numOperacoes];
+    
+    
+    for (size_t i = 0; i < numOperacoes; i++){
+
+    
+    }
+    return 0;
+}
